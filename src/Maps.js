c2032ea6b0fb84ef44e438ef1a2d89388a114264
jason-50302911 <s50302911@gmail.com>
2024-07-05 17:35:07 +0800

    version 3.1

--- src/Maps.js
+++ src/Maps.js
@@ -36,7 +36,7 @@ const Maps = ({ distance })  => {
 
     useEffect(() => {
       if (distance) {
-        if (distance > 1.3) setDefZoom(14);
+        if (distance > 1.3) setDefZoom(13.4);
       } else setDefZoom(16);
     }, [distance, setDefZoom]);
 
